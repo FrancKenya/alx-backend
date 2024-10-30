f1a6d3b2e93f2d2593ee0fb45bcb5beb63ed74dc
Francis Waihiga <francisgwaihiga@gmail.com>
2024-10-30 19:49:45 +0300

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This program contains a Child class that inherits from
+the imported BaseCaching class"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A class that inherits from BaseCaching and implements
+    a caching system using dictionaries as a basic
+    caching system"""
+    def put(self, key, item):
+        """Put an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Used to get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
