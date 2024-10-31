20d0aa01bcf59b3caf4f82c9651308aa0cdded22
Francis Waihiga <francisgwaihiga@gmail.com>
2024-10-31 17:16:51 +0300

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,7 +17,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Put an item in the cache
         """
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
