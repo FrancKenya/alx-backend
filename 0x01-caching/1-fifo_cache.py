e13bc7ed8201ec6e5764e6f0075925877ca07cd5
Francis Waihiga <francisgwaihiga@gmail.com>
2024-10-31 17:15:00 +0300

    Output error corrections

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,7 +14,7 @@ class FIFOCache(BaseCaching):
         """Discards the first item  in the cache and adds
         a new one
         """
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first_key = list(self.cache_data.keys())[0]
