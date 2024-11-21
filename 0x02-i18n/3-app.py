3d89836601372f02ee91dcca1ca3876469720e9e
Francis Waihiga <francisgwaihiga@gmail.com>
2024-11-21 12:54:08 +0300

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Displays the use of gettext and _ to parametrize templates"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+
+class Config:
+    """Configuration class for the flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines the best match for the supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Function rendering the index page with the welcome message"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '5000')
