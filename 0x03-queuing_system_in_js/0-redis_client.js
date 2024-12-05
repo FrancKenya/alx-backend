36b42753c2175e3db00f5476be7c260dadec0f1b
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-06 00:09:03 +0300

    Task 0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -4,15 +4,10 @@ const client = createClient({
   url: 'redis://127.0.0.1:6379',
 });
 
-
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
 client.on('error', (err) => {
   console.error(`Redis client not connected to the server: ${err.toString()}`);
-});
-
-client.connect().catch((err) => {
-  console.error(`Redis client not connected to the server: ${err.message}`);
 });
