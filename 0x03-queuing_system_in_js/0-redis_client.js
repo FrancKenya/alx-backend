65f82cd25a78605b387e1db6932840a30532dd30
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-05 22:55:24 +0300

    Task 0

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+// Create a Redis client
+const client = createClient();
+
+// Event handler for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event handler for errors
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
